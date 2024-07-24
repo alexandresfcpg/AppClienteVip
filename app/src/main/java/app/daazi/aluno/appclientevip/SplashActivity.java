50176375b3c193d823d5551053942360cea7096c
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-07-24 18:48:10 -0300

    Implementando o uso da tela spalsh, trcoando pela tela principal

--- app/src/main/java/app/daazi/aluno/appclientevip/SplashActivity.java
+++ app/src/main/java/app/daazi/aluno/appclientevip/SplashActivity.java
@@ -2,7 +2,11 @@ package app.daazi.aluno.appclientevip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import app.daazi.aluno.appclientevip.api.AppUtil;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        iniciarAplicativo();
+    }
+
+    private void iniciarAplicativo() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, CadastroUsuarioActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        }, AppUtil.TIME_SPLASH);
+
     }
 }
---
+++ app/src/main/java/app/daazi/aluno/appclientevip/api/AppUtil.java
@@ -0,0 +1,6 @@
+package app.daazi.aluno.appclientevip.api;
+
+public class AppUtil {
+
+    public static final int TIME_SPLASH = 5 * 1000;
+}
