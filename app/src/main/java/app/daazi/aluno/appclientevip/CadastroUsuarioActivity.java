80d286703a313ccd7c51723e3fe79d3dd248436b
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-07-25 14:38:43 -0300

    Programando a tela de login

--- app/src/main/java/app/daazi/aluno/appclientevip/CadastroUsuarioActivity.java
+++ app/src/main/java/app/daazi/aluno/appclientevip/CadastroUsuarioActivity.java
@@ -3,12 +3,84 @@ package app.daazi.aluno.appclientevip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    Button btnCadastro;
+
+    EditText editNome, editEmail, editSenhaA, editSenhaB;
+    CheckBox ckTermo;
+    boolean isFormularioOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        initFormulario();
+
+        btnCadastro.setOnClickListener((v -> {
+
+            isFormularioOK = true;
+
+            if(TextUtils.isEmpty(editNome.getText().toString())){
+                editNome.setError("*");
+                editNome.requestFocus();
+                isFormularioOK = false;
+            }
+            if(TextUtils.isEmpty(editEmail.getText().toString())){
+                editEmail.setError("*");
+                editEmail.requestFocus();
+                isFormularioOK = false;
+            }
+            if(TextUtils.isEmpty(editSenhaA.getText().toString())){
+                editSenhaA.setError("*");
+                editSenhaA.requestFocus();
+                isFormularioOK = false;
+            }
+            if(TextUtils.isEmpty(editSenhaB.getText().toString())){
+                editSenhaB.setError("*");
+                editSenhaB.requestFocus();
+                isFormularioOK = false;
+            }
+            if(!ckTermo.isChecked()){
+                isFormularioOK = false;
+            }
+
+            if (isFormularioOK){
+
+                if (!validarSenha()){
+
+                    editSenhaA.setError(("*"));
+                    editSenhaB.setError(("*"));
+                    editSenhaA.requestFocus();
+
+                    Toast.makeText(getApplicationContext(), "As senhas digitadas não conferem...",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }));
+    }
+
+    private boolean validarSenha() {
+
+        return false;
+    }
+
+    private void initFormulario() {
+
+        editNome = findViewById(R.id.editNome);
+        editEmail = findViewById(R.id.editEmail);
+        editSenhaA = findViewById(R.id.editSenhaA);
+        editSenhaB = findViewById(R.id.editSenhaB);
+        ckTermo = findViewById(R.id.ckTermo);
+
+        isFormularioOK = false;
     }
 }
--- app/src/main/java/app/daazi/aluno/appclientevip/LoginActivity.java
+++ app/src/main/java/app/daazi/aluno/appclientevip/LoginActivity.java
@@ -3,12 +3,60 @@ package app.daazi.aluno.appclientevip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView txtRecuperarSenha, txtLerPolitica;
+    EditText editEmail, editSenha;
+    CheckBox ckLembrar;
+    Button btnAcessar, btnSejaVip;
+
+    boolean isFormularioOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initFormulario();
+
+        txtRecuperarSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Carregando tela de recuperação de senha...",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        txtLerPolitica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Carregando tela com a política de privacidade...",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void initFormulario() {
+
+        txtRecuperarSenha = findViewById(R.id.txtRecuperarSenha);
+        txtLerPolitica = findViewById(R.id.txtLerPolitica);
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        ckLembrar = findViewById(R.id.ckLembrar);
+        btnAcessar = findViewById(R.id.btnAcessar);
+        btnSejaVip = findViewById(R.id.btnSejaVip);
+
+        isFormularioOK = false;
     }
 }
