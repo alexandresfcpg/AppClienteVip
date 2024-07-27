fbbc8ad36eeec2ce9bbd8f832f35dd02c50277af
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-07-27 16:06:45 -0300

    Criação das telas de Cadastro de Cliente Pessoa Juridica e Recupração de Senha

---
+++ app/src/main/java/app/daazi/aluno/appclientevip/view/ClientePessoaJuridica.java
@@ -0,0 +1,19 @@
+package app.daazi.aluno.appclientevip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import app.daazi.aluno.appclientevip.R;
+
+public class ClientePessoaJuridica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_pessoa_juridica);
+    }
+
+
+}
---
+++ app/src/main/java/app/daazi/aluno/appclientevip/view/RecuperarSenha.java
@@ -0,0 +1,16 @@
+package app.daazi.aluno.appclientevip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.daazi.aluno.appclientevip.R;
+
+public class RecuperarSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_senha);
+    }
+}
