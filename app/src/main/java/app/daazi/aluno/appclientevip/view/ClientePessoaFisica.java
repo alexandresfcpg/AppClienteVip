7b3bde4b037599ecc60768213bc11deeff6b38b4
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-07-27 16:15:56 -0300

    Criação da tela de Cadastro de Cliente Pessoa Física

---
+++ app/src/main/java/app/daazi/aluno/appclientevip/view/ClientePessoaFisica.java
@@ -0,0 +1,16 @@
+package app.daazi.aluno.appclientevip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.daazi.aluno.appclientevip.R;
+
+public class ClientePessoaFisica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_pessoa_fisica);
+    }
+}
