35b4be1330ede45c51f003e422f02b3c184b0ef0
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-07-29 21:34:06 -0300

    Criando o layout do menu principal e iniciando a programação na classe MainActivity.

--- app/src/main/java/app/daazi/aluno/appclientevip/view/MainActivity.java
+++ app/src/main/java/app/daazi/aluno/appclientevip/view/MainActivity.java
@@ -2,15 +2,67 @@ package app.daazi.aluno.appclientevip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import app.daazi.aluno.appclientevip.R;
+import app.daazi.aluno.appclientevip.api.AppUtil;
+import app.daazi.aluno.appclientevip.model.Cliente;
+import app.daazi.aluno.appclientevip.model.ClientePF;
+import app.daazi.aluno.appclientevip.model.ClientePJ;
 
 public class MainActivity extends AppCompatActivity {
+
+    Cliente cliente;
+    ClientePF clientePF;
+    ClientePJ clientePJ;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFormulario();
+    }
+
+    private void initFormulario() {
+
+        cliente = new Cliente();
+        clientePF = new ClientePF();
+        clientePJ = new ClientePJ();
+
+        restaurarSharedPreferences();
+
+
+    }
+
+    private void salvarSharedPreferences() {
+
+        preferences = getSharedPreferences(AppUtil.PREF_APP, MODE_PRIVATE);
+        SharedPreferences.Editor dados = preferences.edit();
+    }
+
+    private void restaurarSharedPreferences() {
+
+        preferences = getSharedPreferences(AppUtil.PREF_APP, MODE_PRIVATE);
+
+        cliente.setPrimeiroNome(preferences.getString("primeiroNome", "NULO"));
+        cliente.setSobreNome(preferences.getString("sobreNome", "NULO"));
+        cliente.setEmail(preferences.getString("email", "NULO"));
+        cliente.setSenha(preferences.getString("senha", "NULO"));
+        cliente.setPessoaFisica(preferences.getBoolean("pessoaFisica", true));
+
+        clientePF.setCpf(preferences.getString("cpf", "NULO"));
+        clientePF.setNomeCompleto(preferences.getString("nomeCompleto", "NULO"));
+
+        clientePJ.setCnpj(preferences.getString("cnpj", "NULO"));
+        clientePJ.setRazaoSocial(preferences.getString("razaoSocial", "NULO"));
+        clientePJ.setDataAbertura(preferences.getString("dataAbertura", "NULO"));
+        clientePJ.setSimplesNacional(preferences.getBoolean("simplesNacional", false));
+        clientePJ.setMei(preferences.getBoolean("mei", false));
+
     }
 }
