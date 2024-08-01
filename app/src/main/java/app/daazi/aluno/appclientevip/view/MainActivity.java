ec89be8337304ac3be7f67ad93d1c3aed5909215
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-08-01 19:37:29 -0300

    Programando o menu Sair do Aplicativo e testando o seu funcionamento.

--- app/src/main/java/app/daazi/aluno/appclientevip/view/MainActivity.java
+++ app/src/main/java/app/daazi/aluno/appclientevip/view/MainActivity.java
@@ -2,9 +2,17 @@ package app.daazi.aluno.appclientevip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.shashank.sony.fancydialoglib.Animation;
+import com.shashank.sony.fancydialoglib.FancyAlertDialog;
+import com.shashank.sony.fancydialoglib.FancyAlertDialogListener;
 
 import app.daazi.aluno.appclientevip.R;
 import app.daazi.aluno.appclientevip.api.AppUtil;
@@ -64,5 +72,44 @@ public class MainActivity extends AppCompatActivity {
         clientePJ.setSimplesNacional(preferences.getBoolean("simplesNacional", false));
         clientePJ.setMei(preferences.getBoolean("mei", false));
 
+    }
+
+    public void meusDados(View view) {
+    }
+
+    public void atualizarMeusDados(View view) {
+    }
+
+    public void excluirMinhaConta(View view) {
+    }
+
+    public void consultarClientesVip(View view) {
+    }
+
+    public void sairDoAplicativo(View view) {
+
+        FancyAlertDialog.Builder
+                .with(MainActivity.this)
+                .setTitle("SAIR DO APLICATIVO")
+                .setBackgroundColor(Color.parseColor("#303F9F"))  // for @ColorRes use setBackgroundColorRes(R.color.colorvalue)
+                .setMessage("Confirma a sua saída do aplicativo ?")
+                .setNegativeBtnText("RETORNAR")
+                .setPositiveBtnBackground(Color.parseColor("#FF4081"))  // for @ColorRes use setPositiveBtnBackgroundRes(R.color.colorvalue)
+                .setPositiveBtnText("SIM")
+                .setNegativeBtnBackground(Color.parseColor("#4ECA25"))  // for @ColorRes use setNegativeBtnBackgroundRes(R.color.colorvalue)
+                .isCancellable(true)
+                .setAnimation(Animation.POP)
+                .setIcon(R.mipmap.ic_launcher_round, View.VISIBLE)
+                .onPositiveClicked(new FancyAlertDialogListener() {
+                    @Override
+                    public void onClick(Dialog dialog) {
+                        Toast.makeText(MainActivity.this, cliente.getPrimeiroNome() + " , volte sempre e obrigado!", Toast.LENGTH_SHORT).show();
+                        finish();
+                        return;
+                    }
+                })
+                .onNegativeClicked(dialog -> Toast.makeText(MainActivity.this, cliente.getPrimeiroNome() + " , obrigado por continuar a usar o nosso aplicativo!", Toast.LENGTH_SHORT).show())
+                .build()
+                .show();
     }
 }
