77ae590a81a9c9c9e820b93045dd5d36f9226779
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-08-03 19:12:33 -0300

    Criação da classe ClienteAdapter e programações nessa tela e na ConsultarClientesActivity.

--- app/src/main/java/app/daazi/aluno/appclientevip/view/ConsultarClientesActivity.java
+++ app/src/main/java/app/daazi/aluno/appclientevip/view/ConsultarClientesActivity.java
@@ -4,13 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.daazi.aluno.appclientevip.R;
+import app.daazi.aluno.appclientevip.api.ClienteAdapter;
+import app.daazi.aluno.appclientevip.model.Cliente;
 
 public class ConsultarClientesActivity extends AppCompatActivity {
+
+    List<Cliente> clientes;
+    ClienteAdapter adapter;
+    Cliente obj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_clientes);
+
+        clientes = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++){
+
+            obj = new Cliente();
+            obj.setPrimeiroNome("Cliente "+i);
+            obj.setPessoaFisica(i % 2==0);
+
+            clientes.add(obj);
+
+        }
+
+        adapter = new ClienteAdapter(clientes, getApplicationContext());
     }
 }
