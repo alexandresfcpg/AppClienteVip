e79a82ad5a40051ab1eb5ddb513a33e88ce1310b
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-08-03 17:20:10 -0300

    Criação dos layouts para exibir o conteúdo da lista de clientes.

---
+++ app/src/main/java/app/daazi/aluno/appclientevip/view/ConsultarClientesActivity.java
@@ -0,0 +1,16 @@
+package app.daazi.aluno.appclientevip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.daazi.aluno.appclientevip.R;
+
+public class ConsultarClientesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_clientes);
+    }
+}
